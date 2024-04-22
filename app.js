9840db38a422528c86b1de31f35aef5d980b4189
Nawel Bellil <153316848+Nawel-Bellil@users.noreply.github.com>
2024-04-22 15:50:21 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+//import express frame to build server
 const express = require("express");
 
 const cors = require("cors");
+//create instance of express app
 const app = express();
+
 require("dotenv").config();
 app.use(express.json());
 
@@ -16,9 +19,7 @@ const userRoutes = require("./routes/user");
 app.use("/api", userRoutes);
 // Mount routes
 
-// Socket.IO connection handler
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Use the PORT environment variable if set, otherwise default to 3000
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
