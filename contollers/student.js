947d07aa858cb432ab9afca4e022db5ba8d57bde
Nawel Bellil <153316848+Nawel-Bellil@users.noreply.github.com>
2024-05-27 14:35:24 +0100

    edit student controller

--- contollers/student.js
+++ contollers/student.js
@@ -1,156 +1,228 @@
+// controllers/userController.js
+
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-// Create topic associated with a chapter
-async function createTopic(req, res) {
-  const { title, chapterId } = req.body;
+// Import getAllChapters function from chapters controller
+const { getAllChapters } = require("./chapter");
+const { getAllQuizzes } = require("./quizz");
+
+async function createStudent(req, res) {
+  const { email, password, first_name, last_name, matricule_student } =
+    req.body;
+
+  const matricule = parseInt(matricule_student);
 
   try {
-    // Check if the module exists
-    const existingModule = await prisma.module.findUnique({
-      where: {
-        id: moduleId,
-      },
+    // Check if user already exists
+    const userExists = await prisma.user.findFirst({
+      where: { email },
     });
 
-    if (!existingModule) {
-      return res.status(404).json({ error: "Module not found" });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
     }
 
-    // Check if the chapter exists
-    const existingChapter = await prisma.chapter.findUnique({
-      where: {
-        chapter_id: chapterId,
-      },
-    });
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    if (!existingChapter) {
-      return res.status(404).json({ error: "Chapter not found" });
-    }
-
-    // Get the module ID associated with the chapter
-    const moduleId = existingChapter.moduleId;
-
-    const topic = await prisma.topic.create({
+    // Create user
+    // create() method provided by Prisma's user model
+    const user = await prisma.user.create({
       data: {
-        title,
-        chapter: {
-          connect: {
-            chapter_id: chapterId,
-          },
-        },
+        first_name,
+        last_name,
+        email,
+        password: hashedPassword,
       },
     });
+    const student = await prisma.student.create({
+      data: {
+        user: { connect: { id: user.id } },
+        matricule: parseInt(matricule_student),
+      },
+    });
+    console.log(student);
 
-    res.status(201).json({ message: "Topic created successfully", topic });
+    // Retrieve all chapters
+    // const chapters = await getAllChapters();
+
+    // Create token
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+    res.status(201).json({ token, student });
   } catch (error) {
-    console.error("Error creating topic:", error);
-    res.status(500).json({ error: "Server error" });
+    console.error("Error during user registration:", error);
+    res.status(500).json({ message: "Server error" });
   }
 }
 
-// Get all topics for a specific chapter
-async function getTopics(req, res) {
-  const { chapterId } = req.params;
+async function loginStudent(req, res) {
+  const { email, password } = req.body;
 
   try {
-    // Check if the chapter exists
-    const existingChapter = await prisma.chapter.findUnique({
-      where: {
-        chapter_id: chapterId,
-      },
-    });
+    // Find user by email
+    const user = await prisma.user.findUnique({ where: { email } });
 
-    if (!existingChapter) {
-      return res.status(404).json({ error: "Chapter not found" });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
 
-    const topics = await prisma.topic.findMany({
-      where: {
-        chapterId,
-      },
-    });
+    // Check password
+    const isMatch = bcrypt.compareSync(password, user.password);
 
-    res.status(200).json({ topics });
-  } catch (error) {
-    console.error("Error fetching topics:", error);
-    res.status(500).json({ error: "Server error" });
-  }
-}
-
-// Update a topic
-async function updateTopic(req, res) {
-  const { topicId } = req.params;
-  const { title } = req.body;
-  try {
-    const updatedTopic = await prisma.topic.update({
-      where: { id: parseInt(topicId) },
-      data: { title },
-    });
-    res.json(updatedTopic);
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-}
-// Get a single topic by ID
-async function getTopicById(req, res) {
-  const { chapterId, topicId } = req.params;
-
-  try {
-    // Check if the chapter exists
-    const existingChapter = await prisma.chapter.findUnique({
-      where: {
-        chapter_id: chapterId,
-      },
-    });
-
-    if (!existingChapter) {
-      return res.status(404).json({ error: "Chapter not found" });
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    const topics = await prisma.topic.findUnique({
+    // Generate JWT
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+
+    // Respond with token
+    res.json({ token, user });
+
+    // Retrieve all chapters
+    // const chapters = await getAllChapters();
+  } catch (error) {
+    console.error("Login error:", error);
+
+    res.status(500).json({ error: "Error logging in user" });
+  }
+}
+async function getAllUStudent(req, res) {
+  const users = await prisma.student.findMany({
+    select: {
+      user: true,
+      matricule: true,
+    },
+  });
+  res.json(users);
+}
+async function getStudentById(req, res) {
+  const id = req.params.id;
+  try {
+    const student = await prisma.student.findUnique({
       where: {
-        chapterId,
-        topicId,
+        matricule: parseInt(id),
+      },
+    });
+    if (!student) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+    return res.status(200).json({ student });
+  } catch (e) {
+    console.error("Error fetching student:", e);
+    return res.status(500).json({ error: "Server Error" });
+  }
+}
+async function getStudent(req, res) {
+  const id = req.user.id;
+  console.log(id);
+  try {
+    const student = await prisma.student.findFirst({
+      where: {
+        id: parseInt(id),
+      },
+      select: {
+        user: true,
+        id: true,
+        matricule: true,
+      },
+    });
+    if (!student) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+    return res.status(200).json({ student });
+  } catch (e) {
+    console.error("Error fetching student:", e);
+    return res.status(500).json({ error: "Server Error" });
+  }
+}
+async function deleteStudent(req, res) {
+  try {
+    const user = await prisma.student.findUnique({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    if (!user) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+
+    await prisma.student.delete({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    await prisma.student.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: {
+        matricule,
+      },
+    });
+    await prisma.user.delete({
+      where: {
+        id: user.userId,
+      },
+    });
+    return res.status(200).json({ message: "Student deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting student:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+async function updateStudent(req, res) {
+  try {
+    const { id } = req.params; // Student ID
+    const { password } = req.body; // Updated password
+
+    // Find the student by ID
+    const student = await prisma.student.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    // Check if the student exists
+    if (!student) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+    // Update password if provided
+    if (!password) {
+      return res.status(400).json({ error: "New password is required" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await prisma.student.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        password: hashedPassword,
       },
     });
 
-    res.status(200).json({ topics });
+    return res
+      .status(200)
+      .json({ message: "Student password updated successfully" });
   } catch (error) {
-    console.error("Error fetching topics:", error);
-    res.status(500).json({ error: "Server error" });
-  }
-}
-
-// Update a topic
-async function updateTopic(req, res) {
-  const { topicId } = req.params;
-  const { title } = req.body;
-  try {
-    const updatedTopic = await prisma.topic.update({
-      where: { id: parseInt(topicId) },
-      data: { title },
-    });
-    res.json(updatedTopic);
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-}
-// Delete a topic
-async function deleteTopic(req, res) {
-  const { topicId } = req.params;
-  try {
-    await prisma.topic.delete({ where: { id: parseInt(topicId) } });
-    res.status(204).end();
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    console.error("Error updating student password:", error);
+    return res.status(500).json({ error: "Internal server error" });
   }
 }
 
 module.exports = {
-  createTopic,
-  getTopicById,
-  getTopics,
-  updateTopic,
-  deleteTopic,
+  createStudent,
+  loginStudent,
+  getAllUStudent,
+  deleteStudent,
+  updateStudent,
+  getStudentById,
+  getStudent,
 };
